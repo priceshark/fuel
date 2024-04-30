2f567a28d406b1064ec29632ffd4ea5e03bc7b08
Joel Koen <mail@joelkoen.com>
2024-04-30 10:32:08 +1000

    fix: use numeric precision

--- fuel-fetcher/db.sql
+++ fuel-fetcher/db.sql
@@ -2,7 +2,7 @@ create table price (
     state text not null,
     station int not null,
     fuel text not null,
-    price float,
+    price numeric,
     changed_at int not null,
     checked_at int not null,
     primary key (state, station, fuel)
