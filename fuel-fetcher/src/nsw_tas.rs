cedaa670975d6d7a729048553fd585f0a373836a
Joel Koen <mail@joelkoen.com>
2024-05-06 10:34:40 +1000

    feat(fetcher): nsw token cache

--- fuel-fetcher/src/nsw_tas.rs
+++ fuel-fetcher/src/nsw_tas.rs
@@ -1,20 +1,54 @@
+use std::{
+    fs,
+    path::Path,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
 use anyhow::{bail, Result};
 use base64::{engine::general_purpose::STANDARD, Engine};
 use geo::Point;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::{CurrentPrice, Fuel, State, Station};
 
 fn data(client_id: &str, client_secret: &str) -> Result<RawData> {
     let agent = crate::agent();
-    let encoded = STANDARD.encode(format!("{client_id}:{client_secret}"));
-    let auth: AuthResponse = agent.get( "https://api.onegov.nsw.gov.au/oauth/client_credential/accesstoken?grant_type=client_credentials")
-        .set("Authorization", &format!("Basic {encoded}"))
-        .call()?
-        .into_json()?;
+
+    let mut token = None;
+    let path = Path::new("nsw-token-cache.json");
+    if path.exists() {
+        let auth: AuthCache = serde_json::from_str(&fs::read_to_string(path)?)?;
+        let now = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+        if auth.expires_at > (now - 60) {
+            token = Some(auth.access_token);
+        }
+    }
+
+    let token = match token {
+        Some(x) => x,
+        None => {
+            eprintln!("Fetching new token...");
+            let encoded = STANDARD.encode(format!("{client_id}:{client_secret}"));
+            let auth: AuthResponse = agent.get( "https://api.onegov.nsw.gov.au/oauth/client_credential/accesstoken?grant_type=client_credentials")
+                .set("Authorization", &format!("Basic {encoded}"))
+                .call()?
+                .into_json()?;
+            let issued_at: u64 = auth.issued_at.parse()?;
+            let expires_in: u64 = auth.expires_in.parse()?;
+            let expires_at = (issued_at / 1000) + expires_in;
+
+            let auth = AuthCache {
+                access_token: auth.access_token,
+                expires_at,
+            };
+            fs::write(path, serde_json::to_string_pretty(&auth)?)?;
+            auth.access_token
+        }
+    };
+
     let data: RawData = agent
         .get("https://api.onegov.nsw.gov.au/FuelPriceCheck/v2/fuel/prices?states=NSW|TAS")
-        .set("Authorization", &format!("Bearer {}", auth.access_token))
+        .set("Authorization", &format!("Bearer {}", token))
         .set("apikey", client_id)
         // pretty sure these are only used in the response headers so idc
         .set("transactionid", "a")
@@ -66,6 +100,14 @@ pub fn stations(client_id: &str, client_secret: &str) -> Result<Vec<Station>> {
 #[derive(Deserialize)]
 struct AuthResponse {
     access_token: String,
+    expires_in: String,
+    issued_at: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct AuthCache {
+    access_token: String,
+    expires_at: u64,
 }
 
 #[derive(Deserialize)]
